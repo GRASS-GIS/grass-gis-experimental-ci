2f5d73fb7f75eeb9ad85d48929b788b60b81baba
Vaclav Petras <wenzeslaus@gmail.com>
2020-04-06 16:20:02 -0400

    Plain build based on conda
    Patches, dependencies, and steps according to @cmbarton in
    https://github.com/OSGeo/grass/issues/457

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# The make step requires something like:
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
+# further steps additionally require:
+# export PATH="$PATH:$PREFIX/bin"
+
+# fail on non-zero return code from a subprocess
+set -e
+
+if [ -z "$1" ]
+then
+    echo "Usage: $0 PREFIX"
+    exit 1
+fi
+
+export INSTALL_PREFIX=$1
+
+# GRASS GIS
+
+git clone https://github.com/OSGeo/grass.git --branch master --depth=1
+
+cd grass
+
+for FILE in `find ../patches -name '*.patch'`
+do
+    echo "Applying patch $FILE"
+    patch -p0 < $FILE
+done
+
+bash configure.sh
+make GDAL_DYNAMIC=
+make install
